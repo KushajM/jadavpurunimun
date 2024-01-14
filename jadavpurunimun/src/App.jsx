771bfee351aca6f82e093f16ddc56dd114b2d4c0
Kushaj Mallick <mystinkybutt01@gmail.com>
2024-01-14 22:56:16 +0530

    src files

---
+++ jadavpurunimun/src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import Hero from "./Hero.jsx"
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <div>
+    HELLO BROTHER
+      </div>
+  )
+}
+
+export default App
+
