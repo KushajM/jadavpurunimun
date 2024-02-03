3ad73ca1bab71c97e2d597f12998ee5db1dba28c
Kushaj Mallick <mystinkybutt01@gmail.com>
2024-02-03 11:16:07 +0530

    fix pls

--- src/ToggleButton.jsx
+++ src/ToggleButton.jsx
@@ -12,9 +12,9 @@ const ToggleButton = ({ setOpen }) => {
         >
           <motion.path
           className="stroke-black"
-          // strokeWidth="1"
-          // stroke="black"
-          // strokeLinecap="round"
+          strokeWidth="1"
+          stroke="black"
+          strokeLinecap="round"
           variants={{
             closed: { d: "M 2 2.5 L 20 2.5" },
             open: { d: "M 3 16.5 L 17 2.5" },
@@ -22,9 +22,9 @@ const ToggleButton = ({ setOpen }) => {
         />
         <motion.path
           className="stroke-black"
-          // strokeWidth="3"
-          // stroke="black"
-          // strokeLinecap="round"
+          strokeWidth="3"
+          stroke="black"
+          strokeLinecap="round"
           d="M 2 9.423 L 20 9.423"
           variants={{
             closed: { opacity: 1 },
@@ -33,9 +33,9 @@ const ToggleButton = ({ setOpen }) => {
         />
         <motion.path
           className="stroke-black"
-          // strokeWidth="3"
-          // stroke="black"
-          // strokeLinecap="round"
+          strokeWidth="3"
+          stroke="black"
+          strokeLinecap="round"
           variants={{
             closed: { d: "M 2 16.346 L 20 16.346" },
             open: { d: "M 3 2.5 L 17 16.346" },
